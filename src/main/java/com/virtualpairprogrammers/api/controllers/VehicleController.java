149bba6a5ae9c8a4c297c9a41c07e997ed2add29
vijay <vijayasjava@gmail.com>
2020-01-16 10:31:39 +0530

    modified application test message

--- src/main/java/com/virtualpairprogrammers/api/controllers/VehicleController.java
+++ src/main/java/com/virtualpairprogrammers/api/controllers/VehicleController.java
@@ -40,7 +40,7 @@ public class VehicleController
 	 */
 	public String apiTestUrl()
 	{
-		return "<p>Fleetman API Gateway at " + new Date() + "</p>";
+		return "<p>Fleetman API Gateway at " + new Date() + "</p> <p> Developing by Seemakurthi & co";
 	}
 
 	@GetMapping("/history/{vehicleName}")
